853f486bb60b8f7b6c28d643c50b3161604c5a8a
Ruben <kovacevic.ruben@gmail.com>
2024-06-12 23:09:53 +0200

    init: first commit

---
+++ src/main/java/pet/declare/gateway/petdeclarecloudconfig/PetDeclareCloudConfigApplication.java
@@ -0,0 +1,13 @@
+package pet.declare.gateway.petdeclarecloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetDeclareCloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetDeclareCloudConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pet/declare/gateway/petdeclarecloudconfig/PetDeclareCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package pet.declare.gateway.petdeclarecloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetDeclareCloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
